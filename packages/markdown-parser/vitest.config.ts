b97a5eda6280266f23ec90c15b04f079d8719a77
Juan Andres <theaxford@gmail.com>
2026-03-29 15:45:55 +0200

    build(markdown-parser): enable coverage config and scripts

--- packages/markdown-parser/vitest.config.ts
+++ packages/markdown-parser/vitest.config.ts
@@ -3,5 +3,28 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'node',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/__tests__'],
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 90,
+        functions: 90,
+        branches: 90,
+        statements: 90,
+      },
+    },
+
+    alias: [
+      {
+        find: /^@\/(.*)$/,
+        replacement: '/src/$1',
+      },
+      {
+        find: /^@tests\/(.*)$/,
+        replacement: '/src/__tests__/$1',
+      },
+    ],
   },
 })
