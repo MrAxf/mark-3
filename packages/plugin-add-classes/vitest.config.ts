99f323be205a79a3ff649429d709d825d0ece9e6
Juan Andres <theaxford@gmail.com>
2026-03-29 15:46:33 +0200

    chore(monorepo): add test aliases lint config and env typings

--- packages/plugin-add-classes/vitest.config.ts
+++ packages/plugin-add-classes/vitest.config.ts
@@ -3,5 +3,19 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'node',
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: ['src/__tests__'],
+    },
+    alias: [
+      {
+        find: /^@\/(.*)$/,
+        replacement: '/src/$1',
+      },
+      {
+        find: /^@tests\/(.*)$/,
+        replacement: '/src/__tests__/$1',
+      },
+    ],
   },
 })
--- packages/vue/vitest.config.ts
+++ packages/vue/vitest.config.ts
@@ -5,5 +5,19 @@ export default defineConfig({
   plugins: [Vue()],
   test: {
     environment: 'happy-dom',
+    coverage: {
+      include: ['src/**/*.ts', 'src/**/*.vue'],
+      exclude: ['src/__tests__'],
+    },
+    alias: [
+      {
+        find: /^@\/(.*)$/,
+        replacement: '/src/$1',
+      },
+      {
+        find: /^@tests\/(.*)$/,
+        replacement: '/src/__tests__/$1',
+      },
+    ],
   },
 })
